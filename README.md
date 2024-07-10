860f3f16427e761a64c616a3c3f465b9e4f4de29
Kanishk Shah <kanishkshah@Kanishks-MBP.lan>
2024-07-09 17:05:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Hey there! I'm Kanishk, this is my terminal!
+
+Stuff I'm proud of:
+- I've launched an AR app with 100k+ downloads
+- ML apps that save 30% energy on a 4 GW (GigaWatt) Solar plant
+- Teach a graduate game dev class 
+- Oh some full-stack / app-dev work here and there
---
+++ src/utils/bin/commands.ts
@@ -0,0 +1,183 @@
+// List of commands that do not require API calls
+
+import * as bin from './index';
+import config from '../../../config.json';
+
+// Help
+export const help = async (args: string[]): Promise<string> => {
+  const commands = Object.keys(bin).sort().join(', ');
+  var c = '';
+  for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
+    if (i % 7 === 0) {
+      c += Object.keys(bin).sort()[i - 1] + '\n';
+    } else {
+      c += Object.keys(bin).sort()[i - 1] + ' ';
+    }
+  }
+  return `Welcome! Here are all the available commands:
+\n${c}\n
+[tab]: trigger completion.
+[ctrl+l]/clear: clear terminal.\n
+Type 'sumfetch' to display summary.
+`;
+};
+
+// Redirection
+export const repo = async (args: string[]): Promise<string> => {
+  window.open(`${config.repo}`);
+  return 'Opening Github repository...';
+};
+
+// About
+export const about = async (args: string[]): Promise<string> => {
+  return `Hi, I am ${config.name}. 
+Welcome to my website!
+More about me:
+'sumfetch' - short summary.
+'resume' - my latest resume.
+'readme' - my github readme.`;
+};
+
+export const resume = async (args: string[]): Promise<string> => {
+  window.open(`${config.resume_url}`);
+  return 'Opening resume...';
+};
+
+// Donate
+export const donate = async (args: string[]): Promise<string> => {
+  return `thank you for your interest. 
+here are the ways you can support my work:
+- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
+- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
+`;
+};
+
+// Contact
+export const email = async (args: string[]): Promise<string> => {
+  window.open(`mailto:${config.email}`);
+  return `Opening mailto:${config.email}...`;
+};
+
+export const github = async (args: string[]): Promise<string> => {
+  window.open(`https://github.com/${config.social.github}/`);
+
+  return 'Opening github...';
+};
+
+export const linkedin = async (args: string[]): Promise<string> => {
+  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
+
+  return 'Opening linkedin...';
+};
+
+// Search
+export const google = async (args: string[]): Promise<string> => {
+  window.open(`https://google.com/search?q=${args.join(' ')}`);
+  return `Searching google for ${args.join(' ')}...`;
+};
+
+export const duckduckgo = async (args: string[]): Promise<string> => {
+  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
+  return `Searching duckduckgo for ${args.join(' ')}...`;
+};
+
+export const bing = async (args: string[]): Promise<string> => {
+  window.open(`https://bing.com/search?q=${args.join(' ')}`);
+  return `Wow, really? You are using bing for ${args.join(' ')}?`;
+};
+
+export const reddit = async (args: string[]): Promise<string> => {
+  window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
+  return `Searching reddit for ${args.join(' ')}...`;
+};
+
+// Typical linux commands
+export const echo = async (args: string[]): Promise<string> => {
+  return args.join(' ');
+};
+
+export const whoami = async (args: string[]): Promise<string> => {
+  return getRandomCompliment();
+};
+
+const compliments: string[] = [
+  "You have such a positive energy; it's really refreshing to be around someone like you!",
+  "You have a great sense of style; you really know how to put an outfit together!",
+  "I really appreciate how you listen and make people feel heard.",
+  "Your perspective on things is so unique and interesting.",
+  "You have such a warm and friendly smile; it really brightens up the room.",
+  "I admire how confident and genuine you are.",
+  "It's impressive how you stay calm and collected in any situation.",
+  "You have a great sense of humor; you always know how to make people laugh.",
+  "I can tell you're someone who truly cares about others.",
+  "You're incredibly talented at what you do; it's inspiring to see.",
+  "You bring such positive vibes wherever you go; it's contagious!",
+  "You have a knack for making people feel comfortable and welcome.",
+  "Your kindness is so genuine, and it really shows in how you treat others.",
+  "I admire your creativity; you always have such innovative ideas.",
+  "You have a great way of looking at things that makes everything seem simpler.",
+  "Your enthusiasm is infectious; it really motivates those around you.",
+  "I really appreciate how thoughtful and considerate you are.",
+  "You have such a positive outlook on life; it's really inspiring.",
+  "Your dedication and hard work are truly admirable.",
+  "You have a natural talent for bringing people together.",
+  "Your optimism and cheerful attitude make a big difference to everyone around you."
+];
+
+function getRandomCompliment(): string {
+  const randomIndex = Math.floor(Math.random() * compliments.length);
+  return compliments[randomIndex];
+}
+
+export const ls = async (args: string[]): Promise<string> => {
+  return `a
+bunch
+of
+fake
+directories`;
+};
+
+export const cd = async (args: string[]): Promise<string> => {
+  return `unfortunately, i cannot afford more directories.
+if you want to help, you can type 'donate'.`;
+};
+
+export const date = async (args: string[]): Promise<string> => {
+  return new Date().toString();
+};
+
+export const vi = async (args: string[]): Promise<string> => {
+  return `woah, you still use 'vi'? just try 'vim'.`;
+};
+
+export const vim = async (args: string[]): Promise<string> => {
+  return `'vim' is so outdated. how about 'nvim'?`;
+};
+
+export const nvim = async (args: string[]): Promise<string> => {
+  return `'nvim'? too fancy. why not 'emacs'?`;
+};
+
+export const emacs = async (args?: string[]): Promise<string> => {
+  return `you know what? just use vscode.`;
+};
+
+export const sudo = async (args?: string[]): Promise<string> => {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+  return `Permission denied: with little power comes... no responsibility? `;
+};
+
+// Banner
+export const banner = (args?: string[]): string => {
+  return `
+                .--~~,__
+  :-....,-------\`~~'._.'         - ARF! [Says Rocky]
+   \`-,,,  ,_      ;'~U'
+    _,-' ,' \`-__; '--.
+   (_/'~~      ''''(;
+
+Type 'help' to see the list of available commands.
+Type 'sumfetch' to display summary.
+Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+`;
+};
